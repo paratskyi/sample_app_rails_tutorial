6805e3a65cbe59c3017e0818006a1f4c1185967f
Bogdan Paratskiy <bog4242@gmail.com>
2019-12-01 19:37:40 +0200

    generate users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/new'
   # root 'static_pages#home'
   root 'static_pages#home', as: 'home'
   get 'help' => 'static_pages#help', as: 'help'
   get 'about' => 'static_pages#about', as: 'about'
   get 'contact' => 'static_pages#contact', as: 'contact'
+  get 'signup' => 'users#new', as: 'signup'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
