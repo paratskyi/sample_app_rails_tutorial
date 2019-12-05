ec940a6054630296bb821c3ede6fade685606c0a
Bogdan Paratskiy <bog4242@gmail.com>
2019-12-05 17:18:52 +0200

    wrote tests for signup page

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,24 +4,30 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test 'should get home' do
     get home_url
     assert_response :success
-    assert_select 'title', 'Ruby on Rails Tutorial Sample App'
+    assert_select 'title', full_title()
   end
 
   test 'should get help' do
     get help_url
     assert_response :success
-    assert_select 'title', 'Help | Ruby on Rails Tutorial Sample App'
+    assert_select 'title', full_title('Help')
   end
 
   test 'should get about' do
     get about_url
     assert_response :success
-    assert_select 'title', 'About | Ruby on Rails Tutorial Sample App'
+    assert_select 'title', full_title('About')
   end
 
   test 'should get contact' do
     get contact_url
     assert_response :success
-    assert_select 'title', 'Contact | Ruby on Rails Tutorial Sample App'
+    assert_select 'title', full_title('Contact')
+  end
+  
+  test 'should get sign up' do
+    get signup_url
+    assert_response :success
+    assert_select 'title', full_title('Sign up')
   end
 end
