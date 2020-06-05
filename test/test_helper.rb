7c2ac2b89ba6cde2820a2927d5db8e7aa1fffc5c
Bogdan Paratskiy <bog4242@gmail.com>
2020-06-05 20:05:22 +0300

    implementation of login users

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -11,4 +11,8 @@ class ActiveSupport::TestCase
 
   # Add more helper methods to be used by all tests here...
   include StaticPagesHelper
+
+  def is_logged_in?
+    !session[:user_id].nil?
+  end
 end
