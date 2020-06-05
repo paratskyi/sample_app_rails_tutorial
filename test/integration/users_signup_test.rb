e734987578f718eb94f41ed21b7278e4fdf91fb8
Bogdan Paratskiy <bog4242@gmail.com>
2020-06-05 18:58:56 +0300

    performed exercide-section-7

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -14,6 +14,8 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
       post users_path, params: @invalid_user
     end
     assert_template 'users/new'
+    assert_select 'div#error_explanation'
+    assert_select 'div.field_with_errors'
   end
 
   test 'valid signup information' do
@@ -23,5 +25,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
       follow_redirect!
     end
     assert_template 'users/show'
+    assert_not flash.empty?
   end
 end
