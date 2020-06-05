a7db571e1e9145ec310e2ee4e57ce5027ce721d1
Bogdan Paratskiy <bog4242@gmail.com>
2020-06-05 19:13:42 +0300

    generate controller session

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  get 'users/new'
   # root 'static_pages#home'
-  root 'static_pages#home', as: 'home'
-  get 'help' => 'static_pages#help', as: 'help'
-  get 'about' => 'static_pages#about', as: 'about'
-  get 'contact' => 'static_pages#contact', as: 'contact'
-  get 'signup' => 'users#new', as: 'signup'
+  root   'static_pages#home', as: 'home'
+  get    'help' => 'static_pages#help', as: 'help'
+  get    'about' => 'static_pages#about', as: 'about'
+  get    'contact' => 'static_pages#contact', as: 'contact'
+  get    'signup' => 'users#new', as: 'signup'
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
