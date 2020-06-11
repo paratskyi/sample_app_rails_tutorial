657f7f2a8e10ecc1732a357b2bf6a9a60f5c1774
Bogdan Paratskiy <bog4242@gmail.com>
2020-06-11 20:39:47 +0300

    Finish log in/log out

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_191_209_195_413) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+ActiveRecord::Schema.define(version: 2020_06_10_173619) do
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'name'
-    t.string 'email'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.string 'password_digest'
-    t.index ['email'], name: 'index_users_on_email', unique: true
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
+    t.string "remember_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
+
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -15,4 +15,23 @@ class ActiveSupport::TestCase
   def is_logged_in?
     !session[:user_id].nil?
   end
+
+  def log_in_as(user, options = {})
+    password    = options[:password]    || 'password'
+    remember_me = options[:remember_me] || '1'
+    if integration_test?
+      post login_path, params: { session: { email:       user.email,
+                                  password:    password,
+                                  remember_me: remember_me } }
+    else
+      session[:user_id] = user.id
+    end
+  end
+
+  private
+
+    # Возвращает true внутри интеграционных тестов
+    def integration_test?
+      defined?(post_via_redirect)
+    end
 end
