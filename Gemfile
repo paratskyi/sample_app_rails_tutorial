0f306c3d548a4b9ac11316d476fa89dd7355a88e
Bogdan Paratskiy <bog4242@gmail.com>
2020-06-16 18:09:00 +0300

    Finish user edit, update, index, and destroy actions

--- Gemfile
+++ Gemfile
@@ -38,9 +38,13 @@ gem 'jbuilder', '~> 2.7'
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.2', require: false
 
+gem 'bootstrap-will_paginate', '~> 1.0'
+gem 'will_paginate', '~> 3.3'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: %i[mri mingw x64_mingw]
+  gem 'ffaker', '~> 2.15'
 end
 
 group :development do
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create!(name: 'paratskiy',
+             email: 'bog4242@gmail.com',
+             password: '12345678-',
+             password_confirmation: '12345678-',
+             admin: true)
+
+99.times do |_n|
+  name  = FFaker::Name.name
+  email = FFaker::Internet.email
+  password = FFaker::Internet.password
+  User.create!(name: name,
+               email: email,
+               password: password,
+               password_confirmation: password)
+end
