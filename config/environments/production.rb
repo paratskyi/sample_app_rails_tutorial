2e207a4a10ef64639af0f7c98cdaa8959c4b8391
Bogdan Paratskiy <bog4242@gmail.com>
2020-06-17 23:54:24 +0300

    revert "update env"

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -117,7 +117,7 @@ Rails.application.configure do
   ActionMailer::Base.smtp_settings = {
     address: 'smtp.sendgrid.net',
     port: '587',
-    authentication: :login,
+    authentication: :plain,
     user_name: ENV['SENDGRID_USERNAME'],
     password: ENV['SENDGRID_PASSWORD'],
     domain: 'heroku.com',
